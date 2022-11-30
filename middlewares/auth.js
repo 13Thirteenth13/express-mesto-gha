c1d16ad97642b5cbe259900d9e87b32ce492ddd0
Sergey Krivenko <Krivenko.Sergei.V@gmail.com>
2022-11-30 13:07:36 +0300

    fix: 2nd review

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,18 +2,13 @@ import jwt from 'jsonwebtoken';
 import { UnauthorizedError } from '../errors/index.js';
 
 export const auth = (req, res, next) => {
-  const { authorization = '' } = req.headers;
-  if (!authorization) {
+  const token = req.cookies.jwt;
+  const { JWT_SECRET } = req.app.get('config');
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    req.user = payload;
+    next();
+  } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
-  } else {
-    const token = req.cookies.jwt || authorization.replace(/^Bearer*\s*/i, '');
-    const { JWT_SECRET } = req.app.get('config');
-    try {
-      const payload = jwt.verify(token, JWT_SECRET);
-      req.user = payload;
-      next();
-    } catch (err) {
-      next(new UnauthorizedError('Необходима авторизация'));
-    }
   }
 };
