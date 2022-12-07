5456cf0370aa748cccf4907ece6f4939dfa5ea32
Sergey Krivenko <Krivenko.Sergei.V@gmail.com>
2022-12-07 21:44:08 +0300

    fixes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 import pino from 'pino-http';
 import { errors, isCelebrateError } from 'celebrate';
 import { constants } from 'http2';
+import { fileURLToPath } from 'url';
 
 import { router as userRouter } from './routes/users.js';
 import { router as cardRouter } from './routes/cards.js';
@@ -15,22 +16,33 @@ import { router as authRouter } from './routes/auth.js';
 import { HTTPError, NotFoundError } from './errors/index.js';
 import { auth } from './middlewares/auth.js';
 
+export const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const run = async (envName) => {
   process.on('unhandledRejection', (err) => {
     console.error(err);
     process.exit(1);
   });
 
-  const config = dotenv.config({ path: path.resolve('.env.common') }).parsed;
+  const config = dotenv.config({ path: path.resolve(__dirname, '.env.common') }).parsed;
   if (!config) {
     throw new Error('Config не найден');
   }
   config.NODE_ENV = envName;
 
   const app = express();
-  const logger = pino({
-    level: config.LOG_LEVEL,
-  });
+  const loggerConfig = envName.includes('prod')
+    ? { level: config.LOG_LEVEL }
+    : {
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+        },
+      },
+      level: config.LOG_LEVEL,
+    };
+  const logger = pino(loggerConfig);
 
   app.set('config', config);
   app.use(logger);
--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 import { run } from '../app.js';
 
-run(process.env.NODE_ENV)
+run(process.env.NODE_ENV || 'development')
   .catch((err) => {
     console.error(err);
     process.exit(1);
